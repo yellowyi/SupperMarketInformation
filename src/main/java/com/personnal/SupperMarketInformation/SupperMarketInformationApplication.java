0befba71323107842996a5afc74ba15fbb2b01eb
yichenglong <yichenglong@philisense.com>
2019-07-04 10:08:03 +0800

    commit code

---
+++ src/main/java/com/personnal/SupperMarketInformation/SupperMarketInformationApplication.java
@@ -0,0 +1,13 @@
+package com.personnal.SupperMarketInformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupperMarketInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupperMarketInformationApplication.class, args);
+	}
+
+}
